bfd89b6758eb976e7b9a0154861e8633b86a09e2
Talsview <daviconcyline@gmail.com>
2025-10-07 21:06:38 -0300

    Update Violao.java

--- Instrumentos Musicais/Violao.java
+++ Instrumentos Musicais/Violao.java
@@ -1,6 +1,7 @@
 class Violao extends Instrumento {
     @Override
     public void tocar() {
-        System.out.println("Som de violão: *plim plim plim*");
+        System.out.println("plim plim plim");
     }
+
 }
